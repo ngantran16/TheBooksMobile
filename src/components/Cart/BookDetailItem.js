ab5dd41e2363022854621dad00be526d29d4d79f
Tran Thi Tuyet Ngan <ngan.tran21@student.passerellesnumeriques.org>
2021-01-27 14:31:06 +0700

    Cart UI and custom alert

---
+++ src/components/Cart/BookDetailItem.js
@@ -0,0 +1,122 @@
+/* eslint-disable react/jsx-no-undef */
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions, Image } from 'react-native';
+import Colors from '../../themes/Colors';
+import Images from '../../themes/Images';
+import IconStar from '../../components/HomePage/IconStar';
+import Icon from 'react-native-vector-icons/thebook-appicon';
+
+const BookDetailItem = (props) => {
+  const SCREEN_WIDTH = Dimensions.get('screen').width;
+  var iconRatings = [];
+
+  for (let i = 0; i < props.item.rating; i++) {
+    iconRatings.push(<IconStar color={Colors.primary} size={13} />);
+  }
+  for (let i = 0; i < 5 - props.item.rating; i++) {
+    iconRatings.push(<IconStar color={Colors.greyAuthor} size={13} />);
+  }
+  return (
+    <View>
+      <View style={styles.content}>
+        <View>
+          <Image source={Images.bookExample} style={styles.imgBook} />
+        </View>
+        <View style={styles.detail}>
+          <View style={{ marginLeft: SCREEN_WIDTH - 145 }}>
+            <Icon name="ic-delete" size={10} color={Colors.blackIcon} />
+          </View>
+          <Text style={styles.nameBook}>{props.item.name}</Text>
+          <Text style={styles.nameAuthor}>{props.item.author}</Text>
+          <View style={styles.starRating}>
+            <View style={styles.star}>{iconRatings}</View>
+            <Text style={styles.textNumber}>{props.item.numberBuyer}</Text>
+          </View>
+
+          <View style={styles.footerContent}>
+            <View style={styles.quantityContainer}>
+              <Icon name="ic-book-1" size={13} color={Colors.primary} />
+              {props.item.quantity === 0 ? (
+                <Text style={[{}, styles.quantity, { color: Colors.primary }]}>hết sách</Text>
+              ) : (
+                <Text style={styles.quantity}>{props.item.quantity} quyển</Text>
+              )}
+            </View>
+            <View style={styles.priceContainer}>
+              <Icon name="ic-price" size={13} color={Colors.primary} />
+              <Text style={styles.price}>{props.item.price}</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default BookDetailItem;
+
+const styles = StyleSheet.create({
+  content: {
+    marginTop: 25,
+    flexDirection: 'row',
+  },
+  imgBook: {
+    width: 80,
+    height: 120,
+    borderRadius: 2,
+  },
+  detail: {
+    marginLeft: 24,
+  },
+  nameBook: {
+    fontFamily: 'SVN-ProximaNova',
+    fontSize: 16,
+    color: '#4a4a4a',
+    marginTop: 11,
+  },
+  nameAuthor: {
+    fontFamily: 'SVN-ProximaNova',
+    fontSize: 14,
+    color: Colors.greyAuthor,
+  },
+  starRating: {
+    flexDirection: 'row',
+  },
+  star: {
+    flexDirection: 'row',
+  },
+  textNumber: {
+    fontFamily: 'SVN-ProximaNova',
+    fontSize: 10,
+    color: Colors.greyAuthor,
+    marginLeft: 10,
+  },
+  icCancel: {
+    color: Colors.greyAuthor,
+  },
+  quantity: {
+    color: Colors.greyAuthor,
+    fontFamily: 'SVN-ProximaNova',
+    fontSize: 14,
+    fontWeight: '300',
+    marginLeft: 5,
+  },
+  price: {
+    color: Colors.greyAuthor,
+    fontFamily: 'SVN-ProximaNova',
+    fontSize: 14,
+    fontWeight: '300',
+    marginLeft: 5,
+  },
+  quantityContainer: {
+    flexDirection: 'row',
+  },
+  footerContent: {
+    flexDirection: 'row',
+    marginTop: 17,
+  },
+  priceContainer: {
+    flexDirection: 'row',
+    marginLeft: 36,
+  },
+});
---
+++ src/screens/Cart/index.js
@@ -0,0 +1,120 @@
+/* eslint-disable no-alert */
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react/jsx-no-undef */
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, ScrollView } from 'react-native';
+import BookDetailItem from '../../components/Cart/BookDetailItem';
+import Colors from '../../themes/Colors';
+import Icon from 'react-native-vector-icons/thebook-appicon';
+import AwesomeAlert from 'react-native-awesome-alerts';
+
+const Cart = () => {
+  const listBooks = [
+    {
+      id: '1',
+      image:
+        'https://media.wired.com/photos/5be4cd03db23f3775e466767/125:94/w_2375,h_1786,c_limit/books-521812297.jpg',
+      name: 'Tên bạn là gì ?',
+      author: 'Shinkai Makolo',
+      rating: 3,
+      numberBuyer: 324,
+      quantity: 0,
+      price: 36000,
+    },
+    {
+      id: '2',
+      image:
+        'https://media.wired.com/photos/5be4cd03db23f3775e466767/125:94/w_2375,h_1786,c_limit/books-521812297.jpg',
+      name: 'Một lít nước mắt',
+      author: 'Kito aya',
+      rating: 4,
+      numberBuyer: 53,
+      quantity: 4,
+      price: 36000,
+    },
+  ];
+
+
+  const [showAlert, setShowAlert] = useState(false);
+  return (
+    <View style={styles.mainContainer}>
+      <ScrollView style={styles.container}>
+        <View style={styles.header}>
+          <Icon name="ic-back" size={13} color={Colors.blackIcon} />
+          <Text style={styles.title}>Giỏ hàng</Text>
+          <Icon name="ic-trash" size={13} color={Colors.blackIcon} />
+        </View>
+        {listBooks.map((item, key) => {
+          return <BookDetailItem item={item} key={key} />;
+        })}
+      </ScrollView>
+      <View>
+        <TouchableOpacity style={styles.btnPurchase} onPress={() => setShowAlert(true)}>
+          <Text style={styles.textBtn}>Đặt sách</Text>
+        </TouchableOpacity>
+      </View>
+      <AwesomeAlert
+          show={showAlert}
+          showProgress={false}
+          message="Sách đã được đặt thành công
+Bạn có thể theo dõi tại mục cá nhân"
+          closeOnTouchOutside={false}
+          closeOnHardwareBackPress={false}
+          showConfirmButton={true}
+          confirmText="Cám ơn"
+          confirmButtonColor={Colors.primary}
+          onConfirmPressed={() => setShowAlert(false)}
+          messageStyle = {{textAlign: 'center'}}
+          contentContainerStyle = {styles.alertContainer}
+          confirmButtonStyle = {styles.confirmBtn}
+        />
+    </View>
+  );
+};
+
+export default Cart;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  container: {
+    paddingHorizontal: 15,
+    marginTop: 25,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#4a4a4a',
+    fontFamily: 'SVN-ProximaNova',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  textBtn: {
+    fontFamily: 'SVN-ProximaNova',
+    fontSize: 14,
+    fontWeight: '600',
+    color: 'white',
+  },
+  btnPurchase: {
+    height: 55,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary,
+  },
+  alertContainer: { 
+    width: '100%' 
+  },
+  confirmBtn: {
+    width: 105, 
+    height: 30, 
+    justifyContent: 'center', 
+    alignItems: 'center'
+  },
+
+});
